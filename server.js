2a91cbcc825df49432e4c6ef66cb0fb59fd1e0ff
mirandalu2020 <yuanling1007@hotmail.com>
2023-03-20 12:58:52 -0700

    ignore this commit!!! wrong branch

--- server.js
+++ server.js
@@ -3,26 +3,24 @@ const express = require('express');
 const cors = require('cors');
 
 require('dotenv').config();
-const weather = require('./modules/weather');
-const getMovie = require('./modules/movie');
+// const weather = require('./modules/weather');
+// const getMovie = require('./modules/movie');
 const app = express();
 app.use(cors());
+app.use(express.json());
+const mongoose = require('mongoose');
+const Pokemon = require('./models/books.js');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
 
 
-app.get('/weather', weatherHandler);
-app.get('/movie', getMovie );
+// app.get('/weather', weatherHandler);
+// app.get('/movie', getMovie );
 const PORT = process.env.PORT || 3002;
 
-
-function weatherHandler(request, response) {
-  const { lat, lon } = request.query;
-
-  weather(lat, lon)
-    .then(summaries => response.send(summaries))
-    .catch((error) => {
-      console.error(error);
-      response.status(500).send('Sorry. Something went wrong!');
-    });
-}
 
 app.listen(PORT, () => console.log(`Server up on ${process.env.PORT}`));
